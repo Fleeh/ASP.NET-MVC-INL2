68e23f77e5c3fa3f83ae3bba4ab4421f3a7ec18c
Fleeh <turboralle@hotmail.com>
2022-03-26 13:09:24 +0100

    Changes

--- WebApplication14343/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication14343.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
